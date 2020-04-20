2bea72c1a4e8a2eca59f8341acaff97646c06b1c
ENRIQUE MEDINA <EnMedina286@gmail.com>
2020-04-20 16:55:57 +0200

    footer both versions

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver,BreakpointState } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
+  
+  mobile: boolean;
 
-  constructor() { }
+  constructor(public breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.breakpointObserver
+    .observe('(max-width: 679px)')
+    .subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.mobile = true;
+        console.log(this.mobile);
+      } else {
+        this.mobile = false;
+      }
+    });
   }
 
 }
