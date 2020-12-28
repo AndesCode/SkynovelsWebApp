8af61c3f1c17f84d20775b88e52b794ee41e2dc9
andresplzm02 <andrecitoasd@gmail.com>
2020-12-28 15:18:47 +0100

    cambios

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -9,8 +9,8 @@ export class Dev {
 }
 
 export class Prod {
-    url = 'https://snpruebas.skynovels.net';
-    apiURL = 'https://api.snpruebas.skynovels.net';
-    urlNovelsDb = 'https://api.snpruebas.skynovels.net/api';
-    urlCredentialsNovelsDb = 'https://api.snpruebas.skynovels.net/api';
+    url = 'https://beta.skynovels.net';
+    apiURL = 'https://api.beta.skynovels.net';
+    urlNovelsDb = 'https://api.beta.skynovels.net/api';
+    urlCredentialsNovelsDb = 'https://api.beta.skynovels.net/api';
 }
